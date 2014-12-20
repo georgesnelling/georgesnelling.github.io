e0f85657c2fc3cf2e084d786ffac1a053fd8cab9
George Snelling <georgesn@gmail.com>
2014-12-20 12:47:10 -0800

    grunt task to inject shared head

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+//
+// Inject the shared header into all html files
+//
+// Usage:  grunt
+//
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    replace: {
+      dist: {
+        options: {
+          force: true,
+          noProcess: 'src/_*',
+          patterns: [
+            {
+              match: 'includeHead',
+              replacement:  '<%= grunt.file.read("src/_head.html") %>'
+            }
+          ]
+        },
+        files: [
+          {expand: true, flatten: true, src: ['src/*.html'], dest: '.'}
+        ]
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-replace')
+
+  // Default task(s).
+  grunt.registerTask('default', ['replace'])
+}
